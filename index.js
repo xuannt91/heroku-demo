612ab25414586dc11314bbfce9d72f2ec4218218
XuaNT <xuannt@techasians.com>
2020-09-16 11:48:23 +0700

    update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ app.set("view engine","ejs");
 app.set("views","./views");
 app.listen(process.env.PORT || 3000);
 
-app.get('/apple-app-site-association');
+app.get('apple-app-site-association');
 app.get("/",function(req,res){
   res.render("trangchu");
 });
