a0ba0a8dcc038fefe35d406ef0d55bffd6b6e8cb
XuaNT <xuannt@techasians.com>
2020-09-16 12:31:32 +0700

    update

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ app.set("views","./views");
 app.listen(process.env.PORT || 3000);
 
 app.get('/apple-app-site-association');
+app.get( '/duck.png', function( req, res ) {
+
+  fs.readFile( 'duck.png'', function( err, data ) {
+    if ( err ) {
+      console.log( err );
+      return;
+    }
+    res.write( data );
+    return res.end();
+  });
+});
 app.get("/",function(req,res){
   res.render("trangchu");
 });
