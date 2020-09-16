7540cfe5f8f48b4809803fcd70d8b63522092f9a
XuaNT <xuannt@techasians.com>
2020-09-16 10:55:18 +0700

    port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 app.set("view engine","ejs");
 app.set("views","./views");
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 
 app.get("/",function(req,res){
   res.render("trangchu");
