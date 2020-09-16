2bf6a8013d3feb8407a29c4de56a7baa7f3478fb
XuaNT <xuannt@techasians.com>
2020-09-16 12:00:05 +0700

    update

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require("express");
 var app = express();
+var aasa = app.readFileSync(__dirname + '/static/apple-app-site-association');
 app.set("view engine","ejs");
 app.set("views","./views");
 app.listen(process.env.PORT || 3000);
 
-app.get("/apple-app-site-association", (req, res) => {
-  res.redirect(301, "/apple-app-site-association");
+app.get('/apple-app-site-association', function(req, res, next) {
+     res.set('Content-Type', 'application/json');
+     res.status(200).send(aasa);
 });
 app.get("/",function(req,res){
   res.render("trangchu");
