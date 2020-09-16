d2d7ae0607012f1c9257b4095d06fbcf0d105c92
XuaNT <xuannt@techasians.com>
2020-09-16 11:44:05 +0700

    update

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ app.set("view engine","ejs");
 app.set("views","./views");
 app.listen(process.env.PORT || 3000);
 
-
+app.get('/apple-app-site-association', function(request, response) {
+  console.log(`Sending apple-app-site-association`);
+  response.sendFile(path.resolve(__dirname, '../react-ui/build', 'apple-app-site-association'))
+});
 app.get("/",function(req,res){
   res.render("trangchu");
 });
