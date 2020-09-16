ea59c33066282b98e1544a8f93ea3b282e096a03
XuaNT <xuannt@techasians.com>
2020-09-16 13:37:11 +0700

    update

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 var express = require("express");
+var fs = require("fs");
 var app = express();
 app.set("view engine","ejs");
 app.set("views","./views");
 app.listen(process.env.PORT || 3000);
 
-// app.get('/apple-app-site-association');
-app.get( '/duck.png', function( req, res ) {
-
-  fs.readFile( 'duck.png'', function( err, data ) {
-    if ( err ) {
-      console.log( err );
-      return;
-    }
-    res.write( data );
-    return res.end();
-  });
+var aasa = fs.readFileSync(__dirname + '/apple-app-site-association');
+app.get('/apple-app-site-association', function(req, res, next) {
+     res.set('Content-Type', 'application/json');
+     res.status(200).send(aasa);
 });
 app.get("/",function(req,res){
   res.render("trangchu");
